0b01c77ab24400509695ce488b1e4e79085db960
Bill44-NotAlone <denyc9@mail.ru>
2026-08-17 23:38:07 +0300

    feat(project): init project

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const modelValue = ref('')
+</script>
+
+<template>
+  <div class="p-8">
+    <UFormField label="Название">
+      <UInput v-model="modelValue" type="text" placeholder="Введите текст" />
+    </UFormField>
+  </div>
+</template>

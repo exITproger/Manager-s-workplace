18f76a18b7425aae8a548d03c81524dbdeee1562
exITproger <fuvfuv2024@gmail.com>
2026-08-19 10:09:04 +0300

    moon icon
    иконка SVG

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="min-h-screen bg-white flex flex-col items-center justify-center relative px-4 py-8 font-sans">
+
+     <!-- Иконка луны SVG -->
+    <button 
+      class="absolute top-10 right-5 cursor-pointer text-2xl hover:opacity-80 transition-opacity outline-none"
+      @click="toggleTheme"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+      </svg>
+    </button>
     <div class="flex flex-col items-center w-full max-w-[450px] text-center">
       <!-- Заголовок бренда -->
       <h1 class="text-4xl font-medium tracking-widest text-black mb-1">ТОЧКА</h1>

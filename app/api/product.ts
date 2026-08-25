9b345e222f8d51ac6dde7e851f1ac7b7eb55d435
Bill44-NotAlone <denyc9@mail.ru>
2026-08-25 14:53:00 +0300

    fix(page): reformat and solution errors

--- app/api/product.ts
+++ app/api/product.ts
@@ -1,18 +1,18 @@
-import type { Stock } from '~/types/Stock'
-import { token, tokenCookie } from '~/composables/useAuth'
+import type {Stock} from '~/types/Stock'
+import {token, tokenCookie} from '~/composables/useAuth'
 
 export function useProductRequest(id: number) {
-  const config = useRuntimeConfig()
+    const config = useRuntimeConfig()
 
-  return useFetch<Stock>(`/product/${id}`, {
-    key: `product-${id}`,
-    baseURL: config.public.apiBase as string,
-    headers: { Authorization: `Bearer ${token()}` },
-    onResponseError({ response }) {
-      if (response.status === 401) {
-        tokenCookie().value = null
-        navigateTo('/')
-      }
-    }
-  })
+    return useFetch<Stock>(`/product/${id}`, {
+        key: `product-${id}`,
+        baseURL: config.public.apiBase as string,
+        headers: {Authorization: `Bearer ${token()}`},
+        onResponseError({response}) {
+            if (response.status === 401) {
+                tokenCookie().value = null
+                navigateTo('/')
+            }
+        }
+    })
 }

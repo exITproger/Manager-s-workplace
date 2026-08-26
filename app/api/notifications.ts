8ab94c45d06b3e0ed1ec7798810376c8c85f22c5
Bill44-NotAlone <91334887+Bill44-NotAlone@users.noreply.github.com>
2026-08-26 17:10:44 +0300

    Notififcations office (#28)
    * fix(page): notifications.vue
    
    * fix(page): remove comment

--- app/api/notifications.ts
+++ app/api/notifications.ts
@@ -1,19 +1,16 @@
 import type {NotificationListItem} from "~/types/NotificationListItem.ts";
 import {token, tokenCookie} from "~/composables/useAuth.ts";
 
-export function useNotificationsRequest(recipientId: number | undefined) {
+export function useNotificationsRequest(recipientId: MaybeRefOrGetter<number | undefined>) {
     const config = useRuntimeConfig()
-    const queryBody: any = {}
-
-    if (recipientId) {
-        queryBody.recipient_id = recipientId
-    }
 
     return useFetch<NotificationListItem[]>('/notifications', {
         key: 'notifications',
         baseURL: config.public.apiBase as string,
         headers: {Authorization: `Bearer ${token()}`},
-        query: queryBody,
+        query: {
+            recipient_id: recipientId
+        },
         onResponseError({response}) {
             if (response.status === 401) {
                 tokenCookie().value = null
